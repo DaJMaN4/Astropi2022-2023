5dd0bd416a4d7560b41e8a457043c5fbb0a9cacf
Damian <damianwidzisz@gmail.com>
2023-06-09 14:42:42 +0200

    created aligning of images and ability to choose which match to use

--- CV2/scratch.py
+++ CV2/scratch.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+import keyboard
 from matplotlib import pyplot as plt
 
 
@@ -19,15 +20,13 @@ sift = cv.SIFT_create()
 
 
 for x in range(4):
-    number = 20 - x
+    number = 62 - x
 
     img = cv.imread(r'C:\Users\damwid001\PycharmProjects\Astropi2022-2023\Data\Image'+ str(number) +'.jpg', 1)
     img = cv.resize(img, (410, 308)) #(410, 308)
 
-    x_end = x_offset + img.shape[1]
-    y_end = y_offset + img.shape[0]
-
     if imglast is not None:
+
         img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         imglast_gray = cv.cvtColor(imglast, cv.COLOR_BGR2GRAY)
         kp1, des1 = sift.detectAndCompute(img_gray, None)
@@ -41,26 +40,49 @@ for x in range(4):
         for m, n in matches:
             if m.distance < 0.7 * n.distance:
                 good.append(m)
+        num = 0
+        while True:
+            p1 = kp1[good[num].queryIdx].pt
+            p2 = kp2[good[num].trainIdx].pt
 
-        p1 = kp1[good[0].queryIdx].pt
-        p2 = kp2[good[0].trainIdx].pt
-        print(p1, "space", p2)
+            x_offset_in = int(x_offset + p2[0])
+            y_offset_in = int(y_offset + p2[1])
+            x_end_in = x_offset_in + img.shape[1]
+            y_end_in = y_offset_in + img.shape[0]
+
+            print(p1, "space", p2, "Num", num, y_offset, y_offset_in)
+
+            imgb = np.zeros((y_end_in, x_end_in, 3), dtype=np.uint8)
+            imgb[0:y_end_last, 0:x_end_last] = imgb1
+            imgb[y_offset_in:y_end_in, x_offset_in:x_end_in] = img
+
+            cv.imshow('image', imgb)
+            cv.waitKey(0)
+
+            #while not keyboard.is_pressed("y"):
+            #    pass
+
+            if keyboard.is_pressed("q"):
+                x_end = x_end_in
+                y_end = y_end_in
+                y_offset = y_offset_in
+                x_offset = x_offset_in
+                break
+            num += 1
 
 
-    if x == 0:
+    if imglast is None:
+
+        x_end = x_offset + img.shape[1]
+        y_end = y_offset + img.shape[0]
         imgb = np.zeros((y_end, x_end, 3), dtype = np.uint8)
         imgb[0:y_end, 0:x_end] = img
 
-    else:
-        imgb = np.zeros((y_end, x_end, 3), dtype=np.uint8)
-        imgb[0:y_end_last, 0:x_end_last] = imgb1
-        imgb[y_offset:y_end, x_offset:x_end] = img
 
     x_end_last = x_end
     y_end_last = y_end
 
-    x_offset = int(x_offset + p2[0])
-    y_offset = int(y_offset + p2[1])
+    print(x_offset)
 
     imgb1 = imgb
     imglast = img
@@ -75,6 +97,8 @@ for x in range(4):
 cv.imshow('image', imgb)
 
 cv.waitKey(0)
+print("end")
+
 
 """if len(good) > 4:
     src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
